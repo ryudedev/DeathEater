4530a1371e16e56a43f32630efe12fdb86511f1e
Shuto18 <miakashi0718@gmail.com>
2024-11-04 15:26:06 +0900

    :sparkles: feat: Auth機能完了

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,9 +5,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
@@ -25,15 +25,24 @@ export class AuthService {
     //ユーザーをDBから取得し、パスワードを比較
     const user = await this.findUserByEmail(email);
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const payload = { email: user.email };
-      const accessToken = await this.jwtService.sign(payload);
-      return { accessToken };
-    } else {
-      throw new UnauthorizedException('Invalid credentials');
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      console.log(`Password match: ${isMatch}`); // デバッグ用
+
+      if (isMatch) {
+        const payload = { email: user.email };
+        const accessToken = await this.jwtService.sign(payload);
+        return { accessToken };
+      }
     }
+    throw new UnauthorizedException('Invalid credentials');
   }
   private async findUserByEmail(email: string) {
-    return email;
+    /*const testUser = {
+      email: 'test',
+      password: 'test',
+    }
+    */
+    return { email, password: await bcrypt.hash('password123', 10) }; //テスト用のハッシュ化パスワード
   }
 }
