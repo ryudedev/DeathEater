134f552150cf4277402a483d19aaa4456f48df78
Shuto18 <miakashi0718@gmail.com>
2024-11-08 16:10:41 +0900

    :construction: WIP: Setupの作業中

--- api/setup.sh
+++ api/setup.sh
@@ -5,9 +5,17 @@ until pg_isready -h "db" -p 5432 -U "${POSTGRES_USER}"; do
   sleep 1
 done
 
+echo "db connected"
+
+# migrationsディレクトリが存在する場合は削除
+if [ -d "prisma/migrations" ]; then
+  echo "Removing existing migrations directory..."
+  rm -rf prisma/migrations
+fi
+
 # Prismaのマイグレーションとクライアント生成
 pnpm dlx prisma migrate dev --name init
 pnpm dlx prisma generate
 
 # サーバーの起動
-pnpm start:prisma:dev
+pnpm dlx prisma studio
