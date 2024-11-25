cb1215718707c98cde7805dc2a9c909e471180f2
ryudedev <2210441@ecc.ac.jp>
2024-11-25 12:16:59 +0900

    :recycle: refactor: ログイン時の処理をリファクタリング

---
+++ web/app/api/auth/signin/route.ts
@@ -0,0 +1,58 @@
+import { userPool } from '@/lib/cognito' // Cognitoの設定をインポート
+import { AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js' // Cognito関連のクラスをインポート
+import { NextResponse } from 'next/server' // Next.jsのレスポンスヘルパーをインポート
+
+export async function POST(request: Request) {
+  const { email, password } = await request.json() // リクエストボディからemailとpasswordを取得
+
+  // Cognitoユーザーオブジェクトを作成
+  const user = new CognitoUser({
+    Username: email, // ユーザー名（ここではemailを使用）
+    Pool: userPool, // Cognitoのユーザープール情報
+  })
+
+  // 認証詳細オブジェクトを作成
+  const authDetails = new AuthenticationDetails({
+    Username: email, // 認証に使用するユーザー名
+    Password: password, // 認証に使用するパスワード
+  })
+
+  // Promiseを利用して非同期処理をラップ
+  return new Promise((resolve) => {
+    user.authenticateUser(authDetails, {
+      onSuccess: (result) => {
+        const token = result.getIdToken().getJwtToken() // JWTトークンを取得
+        const response = NextResponse.json({
+          message: 'Sign-in successful!',
+        })
+
+        // CookieにJWTトークンとメールアドレスを保存
+        response.cookies.set('jwtToken', token, {
+          httpOnly: true, // クライアント側からのJavaScriptでのアクセスを防止
+          secure: process.env.NODE_ENV === 'production', // HTTPSでのみ送信
+          sameSite: 'strict', // CSRF対策のため、同一サイトでのみ送信
+          path: '/', // Cookieの有効パスを設定
+          maxAge: 60 * 60 * 24, // Cookieの有効期限（例: 1日）
+        })
+
+        response.cookies.set('email', email, {
+          httpOnly: false, // クライアント側からアクセス可能
+          secure: process.env.NODE_ENV === 'production', // HTTPSでのみ送信
+          sameSite: 'strict', // CSRF対策のため、同一サイトでのみ送信
+          path: '/', // Cookieの有効パスを設定
+          maxAge: 60 * 60 * 24, // Cookieの有効期限（例: 1日）
+        })
+
+        resolve(response)
+      },
+      onFailure: (err) => {
+        resolve(
+          NextResponse.json(
+            { error: err.message },
+            { status: 401 }, // エラーメッセージと401ステータスを返す
+          ),
+        )
+      },
+    })
+  })
+}
